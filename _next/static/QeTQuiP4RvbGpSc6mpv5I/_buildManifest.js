20da03747fc063d44b4704458a0a3a8c13341aaf
zarik5 <zarik5@users.noreply.github.com>
2020-12-13 20:16:54 +0000

    deploy: 68c72192eff1c37ba446b9a4cc9af4c196132631

--- _next/static/QeTQuiP4RvbGpSc6mpv5I/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002F16829283.fcd7dbca09080e9d4a16.js","static\u002Fchunks\u002Fpages\u002Findex-bc342d763715dc44d31d.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-259126d29e0f76e4bcd6.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
