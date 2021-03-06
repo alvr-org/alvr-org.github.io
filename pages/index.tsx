a19176e65292ad8be6c36446edd23c02d50a3eb2
Josiah Nunemaker <josiah.nunemaker@gmail.com>
2021-03-05 23:16:41 -0500

    Formatting cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
-import { ArrowCircleRight, Menu, Sparkles, StatusOnline } from 'heroicons-react';
-import Head from 'next/head'
-import React from 'react';
-import { Button } from '../components/Button';
-import { Card } from '../components/Card';
-import { Banner } from '../components/Banner';
-
+import {
+  ArrowCircleRight,
+  CurrencyDollarOutline,
+  Menu,
+  SparklesOutline,
+  StatusOnlineOutline,
+} from "heroicons-react";
+import Head from "next/head";
+import React from "react";
+import { Button } from "../components/Button";
+import { Card } from "../components/Card";
+import { Banner } from "../components/Banner";
 
 export default function Home() {
   return (
@@ -14,40 +19,78 @@ export default function Home() {
         <link rel="icon" href="/favicon.png" />
 
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;600;700&display=swap" rel="stylesheet"></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        ></link>
       </Head>
 
-    <header className="flex justify-between items-center py-2 px-4">
-      <div className="flex items-center space-x-2">
-        <img src="/logo.png" className="h-12" />
-      </div>
-      <Menu className="w-8 h-8 text-blue-500 hidden" />
-    </header>
+      <header className="flex justify-between items-center py-2 px-4">
+        <div className="flex items-center space-x-2">
+          <img src="/logo.png" className="h-12" />
+        </div>
+        <Menu className="w-8 h-8 text-blue-500 hidden" />
+      </header>
 
-    <div className="py-16 px-4 flex flex-col items-center">
-      <h1 className="text-4xl text-center">
-        <span className="text-blueGray-500 font-medium">PC VR</span>
-        <br />
-        <strong className="text-blueGray-300 font-bold tracking-wide">Without the Wires</strong>
-      </h1>
+      <div className="py-16 px-4 flex flex-col items-center">
+        <h1 className="text-4xl text-center">
+          <span className="text-blueGray-500 font-medium">PC VR</span>
+          <br />
+          <strong className="text-blueGray-300 font-bold tracking-wide">
+            Without the Wires
+          </strong>
+        </h1>
 
-        <a href="https://github.com/alvr-org/alvr#requirements" className="mt-16">
-          <Button className="flex items-center space-x-2"><span>Get Started</span><ArrowCircleRight /></Button>
+        <a
+          href="https://github.com/alvr-org/alvr#requirements"
+          className="mt-16"
+        >
+          <Button className="flex items-center space-x-2">
+            <span>Get Started</span>
+            <ArrowCircleRight />
+          </Button>
         </a>
-    </div>
+      </div>
 
-    <div className="p-4 flex flex-col space-y-8 justify-around items-center md:p-8 lg:items-stretch lg:space-x-8 lg:space-y-0 lg:flex-row">
-      <Card icon={<StatusOnline className="w-14 h-14" />} label="Truly Wireless" className="max-w-lg bg-gradient-to-br from-orange-500 via-red-500 to-red-600 flex-1">
-        <p className="opacity-90 text-lg">By streaming games straight from your PC to your headset, ALVR lets you play without a tether. No more tangling and tripping!</p>
-      </Card>
-      <Card icon={<StatusOnline className="w-14 h-14" />} label={<div>100% Free<br/>& Open Source</div>} className="max-w-lg bg-gradient-to-br from-lime-400 via-green-600 to-green-600 flex-1">
-        <p className="opacity-90 text-lg">You can’t beat free. Especially when that free is 100% backed by open-source software for the community to contribute to.</p>
-      </Card>
-      <Card icon={<Sparkles className="w-14 h-14" />} label={<div>SteamVR Compatible</div>} className="max-w-lg bg-gradient-to-br from-lightBlue-400 via-blue-500 to-blue-600 flex-1">
-        <p className="opacity-90 text-lg">Play all the latest and greatest PC VR games on your Quest. If you already have VR games in your Steam library, no need to purchase them again!</p>
-      </Card>
-
-    </div>
+      <main className="flex flex-col items-center">
+        <div className="p-4 xl:px-0 flex flex-col space-y-8 justify-between items-center md:p-8 lg:items-stretch lg:space-x-8 lg:space-y-0 lg:flex-row max-w-screen-xl">
+          <Card
+            icon={<StatusOnlineOutline className="w-14 h-14" />}
+            label="Truly Wireless"
+            className="max-w-lg bg-gradient-to-br from-orange-500 via-red-500 to-red-600 flex-1"
+          >
+            <p className="opacity-90 text-lg">
+              By streaming games straight from your PC to your headset, ALVR
+              lets you play without a tether. No more tangling and tripping!
+            </p>
+          </Card>
+          <Card
+            icon={<CurrencyDollarOutline className="w-14 h-14" />}
+            label={
+              <div>
+                100% Free
+                <br />& Open Source
+              </div>
+            }
+            className="max-w-lg bg-gradient-to-br from-lime-400 via-green-600 to-green-600 flex-1"
+          >
+            <p className="opacity-90 text-lg">
+              You can’t beat free. Especially when that free is 100% backed by
+              open-source software for the community to contribute to.
+            </p>
+          </Card>
+          <Card
+            icon={<SparklesOutline className="w-14 h-14" />}
+            label={<div>SteamVR Compatible</div>}
+            className="max-w-lg bg-gradient-to-br from-lightBlue-400 via-blue-500 to-blue-600 flex-1"
+          >
+            <p className="opacity-90 text-lg">
+              Play all the latest and greatest PC VR games on your Quest. If you
+              already have VR games in your Steam library, no need to purchase
+              them again!
+            </p>
+          </Card>
+        </div>
         <section className="bg-blueGray-600 p-4 text-blueGray-200 my-4 space-y-2 shadow-lg max-w-screen-xl xl:rounded-2xl">
           <p>
             ALVR enables you to stream PC VR games over WiFi to your Oculus
@@ -84,5 +127,5 @@ export default function Home() {
         </p>
       </Banner>
     </div>
-  )
+  );
 }
